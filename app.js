1b2318b9c278cd5cb743d369cf6a8f7ab7c0b9e4
Mazhar Ahmed <ma9822649@gmail.com>
2024-04-21 00:43:20 -0700

    Practice

---
+++ app.js
@@ -0,0 +1,104 @@
+// alert("mazhar ahmed")
+
+/*
+
+
+Operator	Description
+
+Arthematic operators
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ESa016)
+/	Division
+%	Modulus (Remainder)
+++	Increment
+--	Decrement
+
+let a = 2;
+let b = 3;
+
+console.log(a + b); 
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+// a++;
+// b--;
+console.log(++a);
+console.log(--b);
+
+++a;
+--b;
+console.log(a);
+console.log(b);
+*/
+
+// Assignment Operators
+
+// Operator	Example	Same As
+// =	    x = y	  x = y
+// +=	    x += y	  x = x + y
+// -=	    x -= y	  x = x - y
+// *=	    x *= y	  x = x * y
+// /=	    x /= y	  x = x / y
+// %=	    x %= y	  x = x % y
+// **=	    x **= y   x = x ** y
+
+// let  a = 5;
+// a += 4;
+// console.log(a)
+// a -= 4;
+// console.log(a)
+// a *= 4;
+// console.log(a)
+// a /= 4;
+// console.log(a)
+// a %= 4;
+// console.log(a)
+// a **= 4;
+// console.log(a)
+
+
+// Logical  Operators
+
+
+// Operator	Example	    Same As
+// &&=	    x &&= y	    x = x && (x = y)
+// ||=	    x ||= y	    x = x || (x = y)
+
+let x = 5;
+let y = 6;
+ console.log(x < 6 && x == 5)
+ console.log(x > 6 || x == 5)
+ console.log(!true)
+
+
+
+// Comparisons Operators 
+// The chapter JS Comparisons gives a full overview of comparison operators.
+
+// The chapter JS If Else gives a full overview of conditional statements.
+
+// Here are some examples:
+
+// Operator	Description	     Example
+// ==	    equal to	     if (day == "Monday")    
+// >	    greater than	     if (salary > 9000)
+// <	    less than	     if (age < 18)
+
+
+// let a = 5;
+// let b = "5";
+
+// console.log("a == b =", a==b);
+// console.log("a == b =", a!=b);
+// console.log("a == b =", a>b);
+// console.log("a == b =", a<b);
+// console.log("a == b =", a===b);
+
+
+// comments in js 
+// 1 - //
+// 2 - /*   */
